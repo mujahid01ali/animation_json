c8e088c0f40656ef5a63c9d93bccc33dbe8d41d4
Mujahid <mujahid.ali@innobles.com>
2019-10-17 11:36:11 +0530

    Initial Commit

---
+++ JSONANIMATION/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONANIMATION
+//
+//  Created by Mujahid on 17/10/19.
+//  Copyright © 2019 Mujahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
