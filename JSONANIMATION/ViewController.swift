752a83c5bd1597eb22ceeec3bb2fe1391de05972
Mujahid <mujahid.ali@innobles.com>
2020-04-07 12:27:17 +0530

    json animation

--- JSONANIMATION/ViewController.swift
+++ JSONANIMATION/ViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
-
+import Lottie
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let camera = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: nil)
+        self.navigationItem.rightBarButtonItem = camera
+        
+        if let animationView = LOTAnimationView(name: "a") {
+            animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+            animationView.center = self.view.center
+            animationView.contentMode = .scaleAspectFill
+            animationView.loopAnimation = true
+            
+            view.addSubview(animationView)
+        
+            animationView.play()
+        }
         // Do any additional setup after loading the view.
     }
 
